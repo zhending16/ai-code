8e0d219a2fa5aae1489ae8fd5a8fa395eb91541a
17827646956 <2087144438@qq.com>
2025-07-29 13:23:43 +0800

    用户模块后端

--- src/main/java/com/cx/aicode/AiCodeApplication.java
+++ src/main/java/com/cx/aicode/AiCodeApplication.java
@@ -1,11 +1,13 @@
 package com.cx.aicode;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(exposeProxy = true)
+@MapperScan("com.cx.aicode.mapper")
 public class AiCodeApplication {
 
     public static void main(String[] args) {
